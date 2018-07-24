88e0839746f71f27203a2bbd904ca16bb505f985
Tao <I-TAO.CHU@deltaww.com>
2018-07-24 06:54:12 +0000

    modify ConvAutoencoder

---
+++ util/util.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+    
+def plotProgress(history):
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper right')
+    plt.show()
